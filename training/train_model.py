026bb536542abf7e8b7fa505496c27a77c4f5078
Vasco Rodrigues <vascorodrigues1243@gmail.com>
2025-11-17 11:37:55 +0000

    Minor errors fixed and additional libraries added to requirements.txt

--- training/train_model.py
+++ training/train_model.py
@@ -11,7 +11,6 @@ import numpy as np
 import os
 import glob
 import pyarrow.parquet as pq
-from dotenv import load_dotenv
 import os
 
 # ============================================================
